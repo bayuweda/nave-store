311c67d800c02bc76ad94dcbb7dbfb796d1f7df1
bayuweda <bayuweda24@gmail.com>
2026-03-25 18:28:28 +0800

    resolve stuck

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { supabase } from "@/lib/supabase";
 import Footer from "@/components/layout/Footer";
 import Navbar from "@/components/layout/Navbar";
